0e825593396aae2c832b466559d705fe4a295cd1
unknown <jisu1008@inha.edu>
2025-02-18 13:58:51 +0900

    v1.7 day07 stack

--- day07.py
+++ day07.py
@@ -1,14 +1,25 @@
-class Stack:
     def __init__(self):
         self.items = list()
+
     def push(self, data):
+    def push(self, data) -> object:
         self.items.append(data)
+
     def pop(self):
+    def pop(self) -> object:
         return self.items.pop()
+
     def size(self):
+    def size(self) -> int:
         return len(self.items)
+
     def peek(self):
+    def peek(self) -> object:
         return self.items[-1]
+
+    def is_empty(self) -> bool:
+        return len(self.items) == 0
+
 s1 = Stack()
 s1.push(-9)
 s1.push(11)
@@ -16,3 +27,7 @@ s1.push(977)
 print(s1.pop())
 print(s1.peek())
 print(s1.peek())
+print(s1.peek())
+# print(s1.pop())
+# print(s1.pop())
+print(s1.is_empty())
