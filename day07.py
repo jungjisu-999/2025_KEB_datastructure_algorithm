e7356c079d2e1771c1bdad2d422ee8303c5932e3
unknown <jisu1008@inha.edu>
2025-02-18 15:32:07 +0900

    v2.5 day07 queue

--- day07.py
+++ day07.py
@@ -1,6 +1,4 @@
 import random
-
-
 class Node:
     def __init__(self, data, next=None):
         self.data = data
@@ -8,8 +6,12 @@ class Node:
 
 
 class LinkedList:
+class Queue:
     def __init__(self):
         self.head = None
+        self.front = None
+        self.rear = None
+        self._size = 0
 
     def append(self, data):
         if not self.head:
@@ -19,6 +21,15 @@ class LinkedList:
         while current.next:  # if next node exist
             current = current.next  # move
         current.next = Node(data)
+    def enqueue(self, data):
+        self._size = self._size + 1
+        node = Node(data)
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node  # increase rear
 
     def search(self, target) -> bool:
         current = self.head
@@ -43,21 +54,29 @@ class LinkedList:
             else:  # move
                 privious = current
                 current = current.next
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError('dequeue from empty queue')
+        self._size = self._size - 1
+        temp = self.front  # backup
+        self.front = self.front.next  # update
+        if self.front is None:
+            self.real = None
+        return temp.data
 
     def __str__(self):
         node = self.head
-@@ -41,11 +55,21 @@
+        while node is not None:
+            print(node.data)
+            node = node.next
+        return "end"
+
+    def size(self) -> int:
+        return self._size
+
 
 if __name__ == "__main__":
     l = LinkedList()
-    i = 0
-    while i < 20:
-        n = random.randint(1, 20)
-        l.append(n)
-        print(n, end=' ')
-        i = i + 1
-    #print(l)
-    print(l.search(10))
     l.append(7)
     l.append(-11)
     l.append(8)
@@ -75,3 +94,10 @@ if __name__ == "__main__":
     #     i = i + 1
     # #print(l)
     # print(l.search(10))
+    q = Queue()
+    q.enqueue(7)
+    q.enqueue(-11)
+    q.enqueue(8)
+    print(q.size())
+    print(q.dequeue())
+    print(q.size())
