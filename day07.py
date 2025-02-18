7e262051db3871ed2e4ab4ed2e5c28a3844a8c55
unknown <jisu1008@inha.edu>
2025-02-18 15:30:25 +0900

    v2.4 day07 linked list

--- day07.py
+++ day07.py
@@ -1,22 +1,55 @@
 import random
+
+
 class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
-@@ -37,9 +41,11 @@ def __str__(self):
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:  # if next node exist
+            current = current.next  # move
+        current.next = Node(data)
+
+    def search(self, target) -> bool:
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
+
+    def remove(self, target):
+        if self.head.data == target:  # !
+            self.head = self.head.next  # head update
+            print("!")
+            return
+        current = self.head
+        privious = None
+        while current:
+            if current.data == target:
+                privious.next = current.next
+                break  # !
+            else:  # move
+                privious = current
+                current = current.next
+
+    def __str__(self):
+        node = self.head
+@@ -41,11 +55,21 @@
 
 if __name__ == "__main__":
     l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    print(l)
-    print(l.search(999))
-    print(l.search(-11))
     i = 0
     while i < 20:
         n = random.randint(1, 20)
@@ -25,3 +58,20 @@ if __name__ == "__main__":
         i = i + 1
     #print(l)
     print(l.search(10))
+    l.append(7)
+    l.append(-11)
+    l.append(8)
+    print(l)
+    # l.remove(7)
+    # l.remove(-11)
+    l.remove(8)
+    print(l)
+    # l = LinkedList()
+    # i = 0
+    # while i < 20:
+    #     n = random.randint(1, 20)
+    #     l.append(n)
+    #     print(n, end=' ')
+    #     i = i + 1
+    # #print(l)
+    # print(l.search(10))
