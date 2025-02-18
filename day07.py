2154e489a04e5a03bc54290d53ce6d812e4b6583
unknown <jisu1008@inha.edu>
2025-02-18 15:12:45 +0900

    v1.8 day07 stack debugger

--- day07.py
+++ day07.py
@@ -1,21 +1,28 @@
+class Stack:
     def __init__(self):
         self.items = list()
+def a(i):
+    j = 9
+    print(i, j)
 
-    def push(self, data):
     def push(self, data) -> object:
         self.items.append(data)
 
-    def pop(self):
     def pop(self) -> object:
         return self.items.pop()
+def b(k):
+    a(1)
+    print(k)
 
-    def size(self):
     def size(self) -> int:
         return len(self.items)
 
-    def peek(self):
     def peek(self) -> object:
         return self.items[-1]
+def main():
+    print("start")
+    b(3)
+    print("end")
 
     def is_empty(self) -> bool:
         return len(self.items) == 0
@@ -27,7 +34,8 @@ s1.push(977)
 print(s1.pop())
 print(s1.peek())
 print(s1.peek())
-print(s1.peek())
 # print(s1.pop())
 # print(s1.pop())
 print(s1.is_empty())
+if __name__ == "__main__":
+    main()
