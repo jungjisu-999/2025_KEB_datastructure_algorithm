fcfe46870ec5ef72a6a37c7542855d4cfa06e427
unknown <jisu1008@inha.edu>
2025-02-18 15:17:34 +0900

    v2.0 day07 linked list

--- day07.py
+++ day07.py
@@ -1,6 +1,3 @@
-def a(i):
-    j = 9
-    print(i, j)
 class Node:
     def __init__(self, data, next=None):
         self.data = data
@@ -10,13 +7,6 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-def b(k):
-    a(1)
-    print(k)
-def main():
-    print("start")
-    b(3)
-    print("end")
     def append(self, data):
         if not self.head:
             self.head = Node(data)
@@ -26,10 +16,17 @@ def main():
             current = current.next  # move
         current.next = Node(data)
 
+    def __str__(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node = node.next
+        return "end"
 
 if __name__ == "__main__":
-    main()
     l = LinkedList()
     l.append(7)
     l.append(-11)
     l.append(8)
+    l.append(8)
+    print(l)
