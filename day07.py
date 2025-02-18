5fd80424018186c94dc6e2e16b198fce1af216a8
unknown <jisu1008@inha.edu>
2025-02-18 15:27:26 +0900

    v2.2 day07 linked list

--- day07.py
+++ day07.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Node:
     def __init__(self, data, next=None):
         self.data = data
@@ -13,13 +14,20 @@ class LinkedList:
         while current.next:  # if next node exist
             current = current.next  # move
         current.next = Node(data)
+    def search(self, target) -> bool:
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
     def __str__(self):
         node = self.head
         while node is not None:
             print(node.data)
             node = node.next
         return "end"
-from collections import deque
 
 d = deque()
 d.append(7)
@@ -27,10 +35,12 @@ d.append(-11)
 d.append(8)
 
 if __name__ == "__main__":
+    for data in d:
+        print(data)
     l = LinkedList()
     l.append(7)
     l.append(-11)
     l.append(8)
     print(l)
-    for data in d:
-        print(data)
+    print(l.search(999))
+    print(l.search(-11))
