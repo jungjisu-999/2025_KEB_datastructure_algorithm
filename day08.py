ccffa59a1d552d44e791691bda515391b3cbba3f
unknown <jisu1008@inha.edu>
2025-02-19 15:36:04 +0900

    v3.4 day08 dfs

--- day08.py
+++ day08.py
@@ -3,16 +3,12 @@ class TreeNode:
 		self.left = None
 		self.data = None
 		self.right = None
-
-
 if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9]
     root = None
-
     node = TreeNode()
     node.data = numbers[0]
     root = node
-
     for group in numbers[1:]:
         node = TreeNode()
         node.data = group
@@ -28,11 +24,8 @@ if __name__ == "__main__":
                     current.right = node
                     break
                 current = current.right  # move
-
     print("BST 구성 완료")
-
     find_group = int(input())
-
     current = root
     while True:
         if find_group == current.data:
@@ -48,4 +41,30 @@ if __name__ == "__main__":
                 print(f"{find_group}이(가) 존재하지 않습니다")
                 break
             current = current.right
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(visited)
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
+graph = [
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0]
+]
+visited = [0] * len(graph)
+dfs(graph, 4, visited)
+"""
+[0, 0, 0, 0, 0]
+[0, 0, 0, 0, 1] E
+[0, 0, 1, 0, 1] C
+[1, 0, 1, 0, 1] A
+[1, 0, 1, 1, 1] D
+D -> A -> C
+[1, 1, 1, 1, 1] B
+B -> C -> E
+"""
 
