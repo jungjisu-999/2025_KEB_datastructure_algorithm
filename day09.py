252be7cf004df2d022c88b9e47dd3281e00ad681
unknown <jisu1008@inha.edu>
2025-02-20 11:27:08 +0900

    v3.8 day09 tree

--- day09.py
+++ day09.py
@@ -6,29 +6,28 @@ class TreeNode:
 
 
 def insert(root, value):
-    new_node = TreeNode()
-    new_node.data = value
-
     if root is None:
-        return new_node
+        node = TreeNode()
+        node.data = value
+        return node
 
-    current = root
-    while True:
-        if value < current.data:
-            if current.left is None:
-                current.left = new_node
-                break
-            current = current.left  # move
-        else:
-            if current.right is None:
-                current.right = new_node
-                break
-            current = current.right  # move
+    if value < root.data:
+        root.left = insert(root.left, value)
+    else:
+        root.right = insert(root.right, value)
     return root
 
 
 def search(root, value):
-    pass
+    current = root
+    while current:
+        if value == current.data:
+            return current
+        elif value < current.data:
+            current = current.left
+        else:
+            current = current.right
+    return None
 
 
 def post_order(node):
@@ -37,6 +36,33 @@ def post_order(node):
     post_order(node.left)
     post_order(node.right)
     print(f"{node.data} ", end='')
+
+
+def delete(root, value):
+    if root is None:
+        return root
+    if value < root.data:
+        root.left = delete(root.left, value)
+    elif value > root.data:
+        root.right = delete(root.right, value)
+    else:
+        if root.left is None and root.right is None:
+            return None
+        elif root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        else:  # 자식이 두 개인 경우
+            root.data = find_min(root.right).data
+            root.right = delete(root.right, root.data)
+    return root
+
+
+def find_min(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
 
 
 if __name__ == "__main__":
@@ -49,21 +75,21 @@ if __name__ == "__main__":
     print("BST 구성 완료")
     post_order(root)
 
+    print()
+    print(root.data)
+    # find_number = int(input())
+    # if search(root, find_number):
+    #     print(f"{find_number}을(를) 찾았습니다")
+    # else:
+    #     print(f"{find_number}이(가) 존재하지 않습니다")
 
-    # find_group = int(input())
-    #
-    # current = root
-    # while True:
-    #     if find_group == current.data:
-    #         print(f"{find_group}을(를) 찾았습니다")
-    #         break
-    #     elif find_group < current.data:
-    #         if current.left is None:
-    #             print(f"{find_group}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.left
-    #     else:
-    #         if current.right is None:
-    #             print(f"{find_group}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.right
+    delete_number = int(input())
+    if search(root, delete_number):
+        root = delete(root, delete_number)
+        print(f"{delete_number} 삭제 완료")
+    else:
+        print(f"{delete_number}은(는) 트리에 존재하지 않습니다.")
+
+    post_order(root)
+    print()
+    print(root.data)
