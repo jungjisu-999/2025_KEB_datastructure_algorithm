cd99d51fec7e8c2433455c9753e6a0e08aea55b5
unknown <jisu1008@inha.edu>
2025-02-21 20:47:43 +0900

    v0.4

--- BAEKJOON.py
+++ BAEKJOON.py
@@ -1,10 +1,27 @@
-A = int(input())
-B = int(input())
+#11382
+#
+# A,B,C = map( int, input().split())
+# print (A+B+C)
 
-print(A*(B%10))
-print(A*(B%100-B%10)//10) # B%100이 십의자리수로 나와서 문제임
-print(A*(B//100))
-print(A*B)
+#10171
+#고양이 출력
+# print("\\    /\\")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \\(__)|")
+
+#10172
+# print("|\\_/|")
+# print("|q p|   /}")
+# print("( 0 )\"\"\"\\")
+# print("|\"^\"`    |")
+# print("||_/=\\\\__|")
+
+
+
+
+
+
 
 
 
