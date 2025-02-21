ed10c91d440c25a1ee7ca7cd3f57a7e89f0be68c
unknown <jisu1008@inha.edu>
2025-02-21 20:26:23 +0900

    v0.3 2588

--- BAEKJOON.py
+++ BAEKJOON.py
@@ -1,13 +1,10 @@
-#10430
-A,B,C = map(int, input().split())
+A = int(input())
+B = int(input())
 
-
-print((A+B)%C)
-print(((A%C) + (B%C))%C)
-print((A*B)%C)
-print(((A%C) * (B%C))%C)
-
-
+print(A*(B%10))
+print(A*(B%100-B%10)//10) # B%100이 십의자리수로 나와서 문제임
+print(A*(B//100))
+print(A*B)
 
 
 
